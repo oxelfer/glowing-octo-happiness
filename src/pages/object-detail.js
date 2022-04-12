393adc36b0695d8ab422893e1dfc0f108d82dc53
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-04-13 00:32:40 +0100

    API implementation draft

--- src/pages/object-detail.js
+++ src/pages/object-detail.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import './object-detail.css';
@@ -6,7 +7,34 @@ export function ObjectDetail() {
 
   const params = useParams();
 
+  const [piece, setPiece] = useState({});
+  
+  useEffect(() => {
+    
+    fetch(`https://api.artic.edu/api/v1/artworks/${params.id}?fields=id,title,artist_display,date_display,medium_display,department_title,dimensions,category_titles,image_id,alt_text`)
+      .then(response => response.json())
+      .then(data => {
+        setPiece(data.data);
+      });
+
+  }, []);
+    
   return (
-    <h2>Object detail placeholder - {params.id}</h2>
+    <>
+      <h2>Object detail placeholder - {params.id}</h2>
+      <article key={piece['id']} className='object__card'>
+        <header>
+          <h3 className='card__title'>{piece['title']}</h3>
+          <p className='card__author'>{piece['artist_display']}</p>
+        </header>
+
+        <picture className='card__picture'>
+          <img className='card__image' src={`https://www.artic.edu/iiif/2/${piece['image_id']}/full/843,/0/default.jpg`} alt={piece['title']} />
+        </picture>
+
+        <p className='card__ref'> {piece['id']} </p>
+      </article>
+
+    </>
   )
 }

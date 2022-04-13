63bebe015b9f94754681d384f2f046b20c026311
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-04-13 13:09:26 +0100

    creates logo and favicon

--- src/components/header.js
+++ src/components/header.js
@@ -5,17 +5,28 @@ import './header.css';
 export default function Header() {
   return (
     <header className='header'>
+      <picture className='header__picture'>
+        <img className='header__logo' src="/logobasic192mm.png" alt="" />
+      </picture>
 
-      <nav>
-        <ul className='site-nav'>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='collections'>Collections</Link></li>
-          <li><Link to='exhibits'>Exhibits</Link></li>
-          <li><Link to='about'>About</Link></li>
-          <li><Link to='contacts'>Contacts</Link></li>
-          <li><Link to='visit'>Visit</Link></li>
-        </ul>
-      </nav>
+      <div className='header__right'>
+        <div className='header__search'>
+          <p>Placeholder search</p>
+        </div>
+        <nav>
+          <ul className='header__site-nav'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='collections'>Collections</Link></li>
+            <li><Link to='exhibits'>Exhibits</Link></li>
+            <li><Link to='about'>About</Link></li>
+            <li><Link to='contacts'>Contacts</Link></li>
+            <li><Link to='visit'>Visit</Link></li>
+          </ul>
+        </nav>
+
+
+      </div>
+
 
     </header>
   )

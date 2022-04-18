3b7fd700a11a5c7ea5b36377129ff925513db3a5
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-04-18 12:34:43 +0100

    styles the gobackbutton

--- src/components/go-back-button.js
+++ src/components/go-back-button.js
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
 
+import './go-back-button.css';
+
 export default function GoBackButton() {
   const navigate = useNavigate();
 
   return (
-    <button onClick={() => navigate(-1)}>Go back</button>
+    <button className="goback__button" onClick={() => navigate(-1)}>Previous page</button>
   )
 }

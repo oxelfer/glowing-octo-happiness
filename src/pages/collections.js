3b0adf4390d2537bf5801e316243aab98add9748
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-04-24 20:22:02 +0100

    layout for collections and exhibits page

--- src/pages/collections.js
+++ src/pages/collections.js
@@ -19,51 +19,51 @@ export function Collections() {
       <p className='collection__intro'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quae deleniti, delectus accusantium commodi id magni quis voluptatum facilis. Illum necessitatibus consectetur sint, deserunt natus voluptatibus sed soluta quod iusto aliquid nostrum qui ad alias earum esse inventore eum nulla at. Illum pariatur consequatur iure itaque nisi numquam est commodi.</p>
 
       <article className='collection__article'>
-        <picture className='collection__picture'>
-          <Link className='collection__link' to={{ pathname: '/catalogue/cats' }}>
-            <img className='collection__image' src={cat} alt='' />
-          </Link>
-        </picture>
         <Link className='collection__link' to={{ pathname: '/catalogue/cats' }}>
-          <h3 className='collection__title'>Cat collection</h3>
+          <div className='collection__container'>
+            <picture className='collection__picture'>
+              <img className='collection__image' src={cat} alt='' />
+            </picture>
+            <h3 className='collection__title'>Cat collection</h3>
+          </div>
         </Link>
       </article>
-      
+
       <article className='collection__article'>
-        <picture className='collection__picture'>
-          <Link className='collection__link' to={{ pathname: '/catalogue/dogs' }}>
-            <img className='collection__image' src={dog} alt='' />
-          </Link>
-        </picture>
         <Link className='collection__link' to={{ pathname: '/catalogue/dogs' }}>
-          <h3 className='collection__title'>Dog collection</h3>
+          <div className='collection__container'>
+            <picture className='collection__picture'>
+              <img className='collection__image' src={dog} alt='' />
+            </picture>
+            <h3 className='collection__title'>Dog collection</h3>
+          </div>
         </Link>
       </article>
-      
+
       <div className='collection__news'>
         <CarouselNews />
       </div>
 
       <article className='collection__article'>
-        <picture className='collection__picture'>
-          <Link className='collection__link' to={{ pathname: '/catalogue/sunflowers' }}>
-            <img className='collection__image' src={sunflower} alt='' />
-          </Link>
-        </picture>
         <Link className='collection__link' to={{ pathname: '/catalogue/sunflowers' }}>
-          <h3 className='collection__title'>Sunflower collection</h3>
+          <div className='collection__container'>
+            <picture className='collection__picture'>
+              <img className='collection__image' src={sunflower} alt='' />
+            </picture>
+            <h3 className='collection__title'>Sunflower collection</h3>
+          </div>
         </Link>
       </article>
-      
+
       <article className='collection__article'>
-        <picture className='collection__picture'>
-          <Link className='collection__link' to={{ pathname: '/catalogue/fruit' }}>
-            <img className='collection__image' src={fruit} alt='' />
-          </Link>
-        </picture>
         <Link className='collection__link' to={{ pathname: '/catalogue/fruit' }}>
-          <h3 className='collection__title'>Fruit collection</h3>
-        </Link> 
+          <div className='collection__container'>
+            <picture className='collection__picture'>
+              <img className='collection__image' src={fruit} alt='' />
+            </picture>
+            <h3 className='collection__title'>Fruit collection</h3>
+          </div>
+        </Link>
       </article>
 
       <GoBackButton />

3f3549c638f4ad899bfc1e577cf6fbec2c5d5df9
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-04-25 20:12:33 +0100

    carousel component - first drafts

--- src/components/carousel-news.js
+++ src/components/carousel-news.js
@@ -1,7 +1,38 @@
 import './carousel-news.css';
 
+import robin from '../imgs/robin.jpg';
+import lizard from '../imgs/lizard.jpg';
+import workshop from '../imgs/workshop.jpg';
+
 export default function CarouselNews() {
+
   return (
-    <h2>Placeholder - carousel news</h2>
+    <div className='carousel__container'>
+
+      <div className='carousel__slide'>
+        <img className='carousel__image' src={robin} alt='' />
+        <div className='carousel__caption'>Winged paintings</div>
+      </div>
+
+      <div className='carousel__slide'>
+        <img className='carousel__image' src={lizard} alt='' />
+        <div className='carousel__caption'>Lizards in art</div>
+      </div>
+
+      <div className='carousel__slide'>
+        <img className='carousel__image' src={workshop} alt='' />
+        <div className='carousel__caption'>Conservation activities</div>
+      </div>
+
+      <a className='carousel__arrow-left material-symbols-outlined'>
+        chevron_left
+      </a>
+
+      <a className='carousel__arrow-right material-symbols-outlined'>
+        navigate_next
+      </a>
+
+    </div>
+    
   )
 }

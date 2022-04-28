08a03650ba5b610dc671b401c582292822c89458
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-04-28 17:24:02 +0100

    contacts page content and layout

---
+++ src/mocks/contacts-mock.js
@@ -0,0 +1,54 @@
+export default function ContactsMock() {
+  return (
+    [
+      {
+        id: 1,
+        name: 'Mary Doe',
+        email: 'mary.doe@mockup.museum.pt',
+        department: 'Director'
+      },
+      {
+        id: 2,
+        name: 'John Ray',
+        email: 'john.ray@mockup.museum.pt',
+        department: 'Sub-director'
+      },
+      {
+        id: 3,
+        name: 'Adam West',
+        email: 'adam.west@mockup.museum.pt',
+        department: 'Research'
+      },
+      {
+        id: 4,
+        name: 'Terry James',
+        email: 'terry.james@mockup.museum.pt',
+        department: 'Collections'
+      },
+      {
+        id: 5,
+        name: 'Octavia Humperdinck',
+        email: 'octavia.humperdinck@mockup.museum.pt',
+        department: 'Exhibits'
+      },
+      {
+        id: 6,
+        name: 'Albert Jones',
+        email: 'albert.jones@mockup.museum.pt',
+        department: 'Administration'
+      },
+      {
+        id: 7,
+        name: 'August Winter',
+        email: 'august.winter@mockup.museum.pt',
+        department: 'Conservation'
+      },
+      {
+        id: 8,
+        name: 'Elias Pringle',
+        email: 'elias.pringle@mockup.museum.pt',
+        department: 'Shop'
+      }
+    ]
+  )
+}
--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -2,17 +2,33 @@ import Title from '../components/title';
 import IntroParagraph from '../components/intro';
 
 import IntroMock from '../mocks/intro-mock';
+import ContactsMock from '../mocks/contacts-mock';
 
 import './contacts.css';
 
 export function Contacts() {
   const introText = IntroMock()[4];
 
+  const contacts = ContactsMock();
+
   return (
     <>
       <Title text='Contacts' />
       
       <IntroParagraph text={introText.page + ' - ' + introText.text} />
+
+      <div className='contacts-container'>
+      {contacts.map( (item) => {
+        return (
+          <article key={item.id} className='contacts__item'>
+            <p className='contacts__department'>{item.department}</p>
+            <p className='contacts__name'>{item.name}</p>
+            <p className='contacts__email'>{item.email}</p>
+          </article>
+        )
+      })}
+      </div>
+      
     </>
   )
 }

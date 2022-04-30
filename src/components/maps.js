cf04f86a494d6f2cf42ccd1f1ca0691dc11fe228
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-04-30 12:27:28 +0100

    changes embedded map coordinates

--- src/components/maps.js
+++ src/components/maps.js
@@ -3,7 +3,7 @@ import './map.css';
 export default function MapComponent() {
   return (
     <div className='mapcontainer'>
-      <iframe className='map-embed' src='https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3702.565492746327!2d-9.136658357444245!3d38.70692399183759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-PT!2spt!4v1649935018839!5m2!1spt-PT!2spt' width='600' height='450' style={{border:0}} allowFullScreen='' loading='lazy' referrerPolicy='no-referrer-when-downgrade'></iframe>
+      <iframe className='map-embed' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d78082.03514516244!2d-15.908485566154443!3d30.12936467534248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc42e379e6e01b8b%3A0xffacc13a8f1fa74c!2sIlhas%20Selvagens!5e0!3m2!1spt-PT!2spt!4v1651317929936!5m2!1spt-PT!2spt' width='600' height='450' style={{border:0}} allowFullScreen='' loading='lazy' referrerPolicy='no-referrer-when-downgrade'></iframe>
     </div>
   )
 }
